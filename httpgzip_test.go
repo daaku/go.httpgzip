27aff0321b23cd8057898cf14007f0424060b89f
Arnaud Le Blanc <arnaud.lb@gmail.com>
2014-06-27 19:36:39 +0200

    Add a Vary: Accept-Encoding header
    Fixes #3

--- httpgzip_test.go
+++ httpgzip_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -26,6 +27,30 @@ func TestWithoutGzip(t *testing.T) {
 	if l := writer.Body.Len(); l != len(resp) {
 		t.Fatalf("invalid body length, got %d", l)
 	}
+	vary := writer.Header()["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding"}) {
+		t.Fatalf("expected a Vary header with value Accept-Encoding, got %v", vary)
+	}
+}
+
+func TestWithoutGzipWithMultipleVaryHeaders(t *testing.T) {
+	const resp = "hello"
+	handler := httpgzip.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Foo")
+		w.Write([]byte(resp))
+	}))
+	writer := httptest.NewRecorder()
+	handler.ServeHTTP(writer, &http.Request{Method: "GET"})
+	if writer.Body == nil {
+		t.Fatal("expected a body")
+	}
+	if l := writer.Body.Len(); l != len(resp) {
+		t.Fatalf("invalid body length, got %d", l)
+	}
+	vary := writer.Header()["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding", "Foo"}) {
+		t.Fatalf("invalid Vary headers, got %#v", vary)
+	}
 }
 
 func TestWithGzip(t *testing.T) {
@@ -43,6 +68,34 @@ func TestWithGzip(t *testing.T) {
 	if l := writer.Body.Len(); l != 29 {
 		t.Fatalf("invalid body length, got %d", l)
 	}
+	vary := writer.Header()["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding"}) {
+		t.Fatalf("invalid Vary headers, got %#v", vary)
+	}
+}
+
+func TestWithGzipAndMultipleVaryHeader(t *testing.T) {
+	handler := httpgzip.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Foo")
+		w.Write([]byte("hello"))
+	}))
+	writer := httptest.NewRecorder()
+	handler.ServeHTTP(writer, &http.Request{
+		Method: "GET",
+		Header: http.Header{
+			"Accept-Encoding": []string{"gzip"},
+		},
+	})
+	if writer.Body == nil {
+		t.Fatal("expected a body")
+	}
+	if l := writer.Body.Len(); l != 29 {
+		t.Fatalf("invalid body length, got %d", l)
+	}
+	vary := writer.Header()["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding", "Foo"}) {
+		t.Fatalf("invalid Vary headers, got %#v", vary)
+	}
 }
 
 func TestWithGzipReal(t *testing.T) {
@@ -58,6 +111,33 @@ func TestWithGzipReal(t *testing.T) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if string(body) != raw {
 		t.Fatalf(`did not find expected "%s" but got "%s" instead`, raw, resp)
+	}
+	vary := resp.Header["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding"}) {
+		t.Fatalf("invalid Vary headers, got %#v", vary)
+	}
+}
+
+func TestWithGzipRealAndMultipleVaryHeaders(t *testing.T) {
+	const raw = "hello"
+	handler := httpgzip.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Foo")
+		w.Write([]byte(raw))
+	}))
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fatalf("failed http request: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if string(body) != raw {
+		t.Fatalf(`did not find expected "%s" but got "%s" instead`, raw, resp)
+	}
+	vary := resp.Header["Vary"]
+	if !reflect.DeepEqual(vary, []string{"Accept-Encoding", "Foo"}) {
+		t.Fatalf("invalid Vary headers, got %#v", vary)
 	}
 }
 
